a18d25474938c3cf89534d68226b33c635c8020b
Eduardo Molina <eduardo.molina@4clover.cl>
2025-03-21 19:08:48 -0300

    ajusto permisos

--- src/api/auth/auth.service.ts
+++ src/api/auth/auth.service.ts
@@ -62,7 +62,7 @@ export class AuthService {
       role: user.rol,
       value: uuidv4(),
       createToken: dayjs().unix(),
-      permisos: role?.permisos?.length ? role.permisos.map((value) => value.nombre) : [],
+      permisos: role?.permisos?.length ? role.permisos.map((value) => value.name_public) : [],
     };
 
     return pd;
